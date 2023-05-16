d18201f213af806b36014735b1573506f9c5b3ac
Airbornharsh <harshkeshri1234567@gmail.com>
2023-05-16 10:17:17 +0530

    disable database

--- dl_models/settings.py
+++ dl_models/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
--- dl_models/urls.py
+++ dl_models/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("api/", include("api.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
