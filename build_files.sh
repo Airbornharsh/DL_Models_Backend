adbc72dd9e034b6086210f4f5e2b0920b6584933
Airbornharsh <harshkeshri1234567@gmail.com>
2023-05-16 10:39:32 +0530

    Added Activate For VENV

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo = "BUILD START"
 python3 -m pip install pipenv
 pipenv install --system --deploy --ignore-pipfile
+pipenv shell
 python3 manage.py collectstatic --noinput --clear
 echo = "BUILD END"
