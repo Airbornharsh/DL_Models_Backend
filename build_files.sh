210074b56588b6dc0a44e58130967b21c7c1b39f
Airbornharsh <harshkeshri1234567@gmail.com>
2023-05-16 11:18:05 +0530

    .

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo = "BUILD START"
 python3 -m pip install -r requirements.txt
 echo = "PIP INSTALL END"
-python3.11 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 echo = "BUILD END"
