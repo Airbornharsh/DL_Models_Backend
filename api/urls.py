8c634e71409095b64f581b97ecca96960cea05fc
Airbornharsh <harshkeshri1234567@gmail.com>
2023-05-18 13:50:57 +0530

    Added Face Detection Api

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path("flower-detection-5", Flower_Detection_5.as_view()),
+    path("face-expression-detection", Face_Expression_Detection.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from django.core.files.storage import FileSystemStorage
 import os
 import numpy as np
 from PIL import Image
+import cv2
 
 fss = FileSystemStorage()
 
@@ -55,6 +56,61 @@ class Flower_Detection_5(APIView):
 
             return Response(
                 data={"message": "Flower is " + prediction_name}, status=200
+            )
+        except Exception as e:
+            print(e)
+            return Response(data={"message": "error"}, status=400)
+
+
+class Face_Expression_Detection(APIView):
+    def post(self, request):
+        def get_emotion(index):
+            if index == 0:
+                return "angry"
+            elif index == 1:
+                return "disgust"
+            elif index == 2:
+                return "fear"
+            elif index == 3:
+                return "happy"
+            elif index == 4:
+                return "neutral"
+            elif index == 5:
+                return "sad"
+            else:
+                return "surprise"
+
+        try:
+            model_path = "api/models/Face_Expression_Detection/face_expression-v1.h5"
+            model = tf.keras.models.load_model(
+                model_path, custom_objects={"KerasLayer": hub.KerasLayer}
+            )
+            print(request.FILES)
+
+            files = request.FILES["files"]
+
+            folder_location = "./api/images/Face_Expression_Detection/"
+            file_location = folder_location + files.name
+
+            if not os.path.exists("./api/images"):
+                os.makedirs("./api/images")
+            if not os.path.exists(folder_location):
+                os.makedirs(folder_location)
+            fss.save(file_location, files)
+
+            img = cv2.imread(file_location, cv2.IMREAD_GRAYSCALE)
+
+            os.remove(file_location)
+
+            img = cv2.resize(img, (48, 48))
+            new_img = np.asarray(img) / 255.0
+
+            predictions = model.predict(new_img.reshape(1, 48, 48, 1))
+
+            prediction = np.argmax(predictions)
+
+            return Response(
+                data={"message": "Emotion is " + get_emotion(prediction)}, status=200
             )
         except Exception as e:
             print(e)
