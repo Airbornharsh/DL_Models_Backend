603b29868e9cad03ac42cd58c66bc9cd29561cf7
Airbornharsh <harshkeshri1234567@gmail.com>
2023-05-22 21:28:47 +0530

    .

--- dl_models/urls.py
+++ dl_models/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+from settings import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,5 +10,6 @@ urlpatterns = [
     path("api/", include("api.urls")),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
